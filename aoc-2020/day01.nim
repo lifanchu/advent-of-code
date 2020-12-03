d18a44425d9c7d3dadb0feb3bd415e4ff4fb1c1b
root <charles@boxofmanga.com>
2020-12-03 23:16:25 +0900

    Refactor

--- aoc-2020/day01.nim
+++ aoc-2020/day01.nim
@@ -1,37 +1,31 @@
-import strutils
-
-const s = readFile("./inputs/01.txt").strip
+import strutils, unittest
 
 proc stringToInts(s: string): seq[int] =
   for line in s.splitLines:
     result.add line.parseInt
 
 proc fixExpenses(nums: seq[int]): int =
-  for i in nums:
-    for j in nums:
-      if i + j == 2020:
-        return i * j
+  for i in 0..nums.high:
+    for j in i+1..nums.high:
+      if nums[i] + nums[j] == 2020:
+        return nums[i] * nums[j]
 
 proc fixExpensesPart2(nums: seq[int]): int =
-  for i in nums:
-    for j in nums:
-      for k in nums:
-        if i + j + k == 2020:
-          return i * j * k
+  for i in 0..nums.high:
+    for j in i+1..nums.high:
+      for k in j+1..nums.high:
+        if nums[i] + nums[j] + nums[k] == 2020:
+          return nums[i] * nums[j] * nums[k]
 
 when isMainModule:
-  import unittest
 
-  var nums = stringToInts(s)
+  const s = readFile("inputs/day01.txt").strip
+  let nums = stringToInts(s)
+
   let part1 = fixExpenses(nums)
   test "part1":
     check part1 == 988771
 
   let part2 = fixExpensesPart2(nums)
-  echo fixExpensesPart2(nums)
-  # test "part2":
-  #   discard
-
-  test "lol":
-    let foo = stringToInts("123\n456\n-42")
-    check foo == @[123, 456, -42]
+  test "part2":
+    check part2 == 171933104
