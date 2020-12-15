b667b1a666cc2f99142da8a43996866a8f575d87
root <53249934+lifanchu@users.noreply.github.com>
2020-12-15 14:57:10 +0900

    Day 15: Add README and improve readability

---
+++ aoc-2020/15/README.md
@@ -0,0 +1,7 @@
+Initial part 2 solution time:
+
+`CPU Time [] 20.355s`
+
+After some improvements:
+
+`CPU Time [] 17.293s`
--- aoc-2020/15/day15.nim
+++ aoc-2020/15/day15.nim
@@ -1,42 +1,29 @@
-import unittest, strutils, tables, bitops, strscans, sequtils
+import unittest, strutils, tables, sequtils
 from ../../helpers import benchmark
 
-# read from numbers
-# consider most recent number
-# if first time spoken, say 0
-# else announce distance from prev spoken
-
-type
-  Spoken = tuple
-    onePrev, twoPrev: int
-
-proc spokenTwice(t: (int, int)): bool =
-  result = t[0] != t[1]
-  
 proc solve(input: seq[int], n: int): int =
-  var spoken = initTable[int, Spoken]()
+  var spoken = initTable[int, (int, int)]()
+
   for i, n in input:
     spoken[n] = (i + 1, i + 1) 
   var lastSpoken = input[^1]
+
   for i in (input.len+1)..n:
-    # let currentNum = input[i mod input.len]
-    if not spoken.hasKey(lastSpoken) or not spokenTwice(spoken[lastSpoken]):
-      let (twoPrev, onePrev) = spoken[0]
-      spoken[0] = (onePrev, i) #overwrite
-      lastSpoken = 0
-    else:
-      let (twoPrev, onePrev) = spoken[lastSpoken]
-      lastSpoken = onePrev - twoPrev
+    let newSpoken = 
       if not spoken.hasKey(lastSpoken):
-        spoken[lastSpoken] = (i, i)
+        0
       else:
         let (twoPrev, onePrev) = spoken[lastSpoken]
-        spoken[lastSpoken] = (onePrev, i) #overwrite
+        onePrev - twoPrev
+    spoken[newSpoken] =
+      if not spoken.hasKey(newSpoken):
+        (i, i)
+      else:
+        (spoken[newSpoken][1], i)
+    lastSpoken = newSpoken
+
   result = lastSpoken
       
-      
-
-
 when isMainModule:
   const input = staticRead("day15.txt").strip.split(',').map(parseInt)
   
